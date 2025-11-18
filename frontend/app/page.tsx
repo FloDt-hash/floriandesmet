dff1fcd593384b07cb128c7991c417c187e129a7
FloDt-hash <contact.floriandesmet@gmail.com>
2025-11-18 18:40:22 +0100

    HERO changes

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -4,12 +4,18 @@ export default function Home() {
   return (
     <main>
       <div className={styles.hero}>
-        <h1>{"< Florian Desmet />"}</h1>
-        <p>
-          Développeur web freelance. J&apos;accompagne vos projets web de A à Z
+        <h1>
+          Florian <span className={styles.name}>Desmet</span>
+        </h1>
+        <p className={styles.catchphrase}>
+          Développeur web freelance. Je vous accompagne dans vos projets web de
+          A à Z.
         </p>
         <div className="button-border-wrap">
-          <button className="call-to-action">Parlons-en ↗</button>
+          <button className="call-to-action">
+            Parlons-en{" "}
+            <span className="material-symbols-outlined">arrow_outward</span>
+          </button>
         </div>
       </div>
     </main>
