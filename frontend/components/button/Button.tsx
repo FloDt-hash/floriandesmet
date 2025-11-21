fb926a0f1841f0f6e890181bdff91a9136de6abf
FloDt-hash <contact.floriandesmet@gmail.com>
2025-11-21 15:58:15 +0100

    refactoring/variable css

--- frontend/components/button/Button.tsx
+++ frontend/components/button/Button.tsx
@@ -4,10 +4,7 @@ import styles from "./button.module.css";
 export default function Button({ children }: { children: React.ReactNode }) {
   return (
     <div className={styles.buttonBorderWrap}>
-      <button className={styles.button}>
-        {children}{" "}
-        <span className="material-symbols-outlined">arrow_outward</span>
-      </button>
+      <button className={styles.button}>{children}</button>
     </div>
   );
 }
--- frontend/components/hero/Hero.tsx
+++ frontend/components/hero/Hero.tsx
@@ -4,12 +4,9 @@ import Button from "../button/Button";
 export default function Hero() {
   return (
     <div className={styles.hero}>
-      <h1 className={styles.heroTitle}>
-        Florian <span className={styles.name}>Desmet</span>
-      </h1>
+      <h1 className={styles.heroTitle}>Développeur web freelance</h1>
       <p className={styles.catchphrase}>
-        Développeur web freelance. Je vous accompagne dans vos projets web de A
-        à Z.
+        Je vous accompagne dans vos projets web de A à Z.
       </p>
       <Button>Parlons-en</Button>
     </div>
